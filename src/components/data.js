52b4b44230936ae347a91528239b95b578cfe6e1
Gwgos <tinagwgos@gmail.com>
2020-06-13 19:16:36 +0300

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,34 @@
+export const tourData = [
+    {
+        id: 1,
+        city:"London",
+        img: "./images/london.jpg",
+        name: "london bridge",
+        info: 
+            "lorem ipsum dolor sit amet consecretur adipusucing elit. Vel,rependus!"
+    },
+    {
+        id: 2,
+        city:"Tokyo",
+        img: "./images/tokyo.jpg",
+        name: "tokyo town",
+        info: 
+            "lorem ipsum dolor sit amet consecretur adipusucing elit. Vel,rependus!"
+    },
+    {
+        id: 3,
+        city:"Las Vegas",
+        img: "./images/lasvegas.jpg",
+        name: "LA",
+        info: 
+            "lorem ipsum dolor sit amet consecretur adipusucing elit. Vel,rependus!"
+    },
+    {
+        id: 4,
+        city:"New York",
+        img: "./images/NY.jpg",
+        name: "NY",
+        info: 
+            "lorem ipsum dolor sit amet consecretur adipusucing elit. Vel,rependus!"
+    }
+]
